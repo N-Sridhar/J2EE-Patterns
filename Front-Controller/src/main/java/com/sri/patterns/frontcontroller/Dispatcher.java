9386f3f0d67e539cf92b4ecaba6c49f8da7f09d3
N-Sridhar <nsridharbtech@gmail.com>
2021-09-02 20:31:04 +0530

    Front Controller

---
+++ Front-Controller/src/main/java/com/sri/patterns/frontcontroller/Dispatcher.java
@@ -0,0 +1,31 @@
+package com.sri.patterns.frontcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Dispatcher {
+
+	public void dispatch(HttpServletRequest request, HttpServletResponse response, String view) {
+
+		if (view != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(mapToView(view));
+			try {
+				dispatcher.forward(request, response);
+			} catch (ServletException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private String mapToView(String view) {
+		if (view.equals("showStudentDetails")) {
+			return "viewStudentDetails.jsp";
+		}
+		return null;
+	}
+
+}
